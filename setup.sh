736aa428bbb25ae72ad6f3b81414488a1495dfe1
Emiliano Rebolledo <rebolledonavarreteemiliano@gmail.com>
2026-05-11 13:56:40 -0600

    First test to read s3 api-football, and try to use notebook

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@
 export SPARK_HOME=/opt/spark
 export PYTHONPATH=$SPARK_HOME/python/:$SPARK_HOME/python/lib/py4j-0.10.9.7-src.zip:$PYTHONPATH
 export SPARK_LOCAL_IP=127.0.0.1
+export PYSPARK_SUBMIT_ARGS="--packages org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 pyspark-shell"
